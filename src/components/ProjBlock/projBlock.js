7f6290f4f945764aa281164bff43f4b00da7c5cc
kayaktavist <imagerybyar@gmail.com>
2019-03-28 09:53:52 -0700

    cleaned portfolio

--- src/components/ProjBlock/projBlock.js
+++ src/components/ProjBlock/projBlock.js
@@ -2,9 +2,7 @@ import React from "react";
 
 function ProjBlock() {
     return (
-
         <div className="container projects">
-
             <div className="row mt-5">
                 <div className="col">
                     <h1>Recent Projects</h1>
@@ -12,41 +10,20 @@ function ProjBlock() {
             </div>
             <div className="row mt-5">
                 <div className="col-sm-1"></div>
-                <div className="col-sm-3"><a href="https://kayaktavist.github.io/project-one/"> <img className="img-fluid max-width:100 height:auto" src="images/adaJams.png" alt="placeholder" />ADA Jams</a>
-                </div>
-                <div className="col-sm-3"><a href="https://kayaktavist.github.io/TriviaGame/"><img className="img-fluid max-width:100 height:auto" src="images/trivia.png" alt="placeholder" />Trivia Game</a></div>
-                <div className="col-sm-3"> <a href="https://kayaktavist.github.io/Train-demo/"> <img className="img-fluid max-width:100 height:auto" src="images/train.png" alt="placeholder" />Train Scheduler</a></div>
+                <div className="col-sm-3"><a href="https://kayaktavist.github.io/project-one/"> <img className="img-fluid max-width:100 height:auto" src="images/adaJams.png" alt="placeholder" />ADA Jams</a></div>
+                <div className="col-sm-3"> <a href="https://kayaktavist.github.io/Giphy/"> <img className="img-fluid max-width:100 height:auto" src="images/giphy.png" alt="placeholder" />Giph Generator</a></div>
+                <div className="col-sm-3"><a href="https://github.com/kayaktavist/FriendFinder"> <img className="img-fluid max-width:100 height:auto" src="images/home.png" alt="placeholder" />Friend Finder</a></div>
                 <div className="col-sm-3"></div>
             </div>
             <div className="row">
                 <div className="col-12 spacerDiv"></div>
             </div>
-
-            <div className="row mt-5">
-                <div className="col-sm-1"></div>
-                <div className="col-sm-3"> <a href="https://kayaktavist.github.io/Psychic-Game/"> <img className="img-fluid max-width:100 height:auto" src="images/letterGuess.png" alt="placeholder" />Letter Guess Game</a></div>
-                <div className="col-sm-3"> <a href="https://kayaktavist.github.io/Giphy/"> <img className="img-fluid max-width:100 height:auto" src="images/giphy.png" alt="placeholder" />Giph Generator</a></div>
-                <div className="col-sm-3"><a href="https://github.com/kayaktavist/FriendFinder"> <img className="img-fluid max-width:100 height:auto" src="images/home.png" alt="placeholder" />Friend Finder</a></div>
-                <div classn="col-sm-1"></div>
-            </div>
-
-            <div className="row mt-5">
-                <div className="col-sm-1"></div>
-                <div className="col-sm-3"> <a href="https://calm-depths-33859.herokuapp.com/"> <img className="img-fluid max-width:100 height:auto" src="images/Critters.png" alt="placeholder" />Critters</a></div>
-                <div className="col-sm-3"> <a href="https://kayaktavist.github.io/Giphy/"> <img className="img-fluid max-width:100 height:auto" src="images/giphy.png" alt="placeholder"/>Giph Generator</a></div>
-                <div className="col-sm-3"><a href="https://github.com/kayaktavist/FriendFinder"> <img className="img-fluid max-width:100 height:auto" src="images/home.png" alt="placeholder"/>Friend Finder</a></div>
-                <div className="col-sm-1"></div>
-            </div>
             <div class="row">
                 <div class="col-12 mb-5 github">
-                <a href="https://github.com/kayaktavist"><h3>GitHub</h3></a>
+                    <a href="https://github.com/kayaktavist"><h3>GitHub</h3></a>
                 </div>
             </div>
-
-
-            
         </div>
-
     );
 }
 
